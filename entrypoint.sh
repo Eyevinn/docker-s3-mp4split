51d927dce2e170e797a32c0393d09d6778993369
Jonas Birmé <jonas.birme@eyevinn.se>
2022-12-07 12:35:20 +0100

    feat: provide s3 access key for reading (#3)
    * feat: pass through aws access creds as s3 access key to mp4split
    
    * chore: updated documentation

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ usage()
 }
 
 args=()
+s3_access_args=()
+
 eval set -- "$@"
 while [[ $# > 0 ]]; do
   case "$1" in
@@ -14,12 +16,24 @@ while [[ $# > 0 ]]; do
       OUTPUT=$2
       shift
       ;;
+    -*s3_access*)
+      s3_access_args+=($1)
+      ;;
+    -*s3_secret*)
+      s3_access_args+=($1)
+      ;;
     *)
       args+=($1)
       ;;
   esac
   shift
 done
+
+if [ ${#s3_access_args[@]} == 0 ]; then
+  if [ ! -z "$AWS_ACCESS_KEY_ID" ]; then
+    s3_access_args+=(--s3_access_key=$AWS_ACCESS_KEY_ID --s3_secret_key=$AWS_SECRET_ACCESS_KEY)
+  fi
+fi
 
 if [ -z "$OUTPUT" ]; then
   usage
@@ -29,6 +43,6 @@ echo "Writing to $OUTPUT"
 echo "mp4split args: ${args[*]}"
 
 set -o pipefail; \
-  UspLicenseKey=$USP_LICENSE_KEY mp4split -o stdout:.temp ${args[*]} | \
+  UspLicenseKey=$USP_LICENSE_KEY mp4split -o stdout:.temp ${s3_access_args[*]} ${args[*]} | \
   aws s3 cp - $OUTPUT || \
   aws s3 rm $OUTPUT
